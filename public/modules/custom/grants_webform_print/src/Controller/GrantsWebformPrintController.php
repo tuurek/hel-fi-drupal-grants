ada5f0928ce393049fc017b446067d311fa852d1
Petri Leinonen <petri.leinonen@siili.com>
2021-11-25 15:47:30 +0200

    webform print module

---
+++ public/modules/custom/grants_webform_print/src/Controller/GrantsWebformPrintController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\grants_webform_print\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\webform\Entity\Webform;
+
+/**
+ * Undocumented class.
+ */
+class GrantsWebformPrintController extends ControllerBase {
+  /**
+   * The webform request handler.
+   *
+   * @var \Drupal\webform\WebformRequestInterface
+   */
+  protected $requestHandler;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->renderer = $container->get('renderer');
+    $instance->messageManager = $container->get('webform.message_manager');
+    $instance->requestHandler = $container->get('webform.request');
+    return $instance;
+  }
+
+  /**
+   * Does the transformations for the Element array of the form.
+   *
+   * @param $item
+   *   The reference to the item of the key-item pair.
+   * @param string $key
+   *   The key associated with the item above.
+   */
+  private function formatWebformElement(&$item, string $key) {
+    if ($key === '#type' && $item === 'webform_wizard_page') {
+      $item = 'container';
+    }
+    elseif ($key === '#type' && $item === 'select') {
+      $item = 'checkboxes';
+    }
+    elseif ($key === '#type' && $item === 'radios') {
+      $item = 'checkboxes';
+    }
+    if (!str_contains($key, '#')) {
+    }
+  }
+  private function traverseWebform(&$webformArray) {
+    foreach ($webformArray as $key => &$item) {
+      if (is_array($item)) {
+        $this->traverseWebform($item);
+        if (isset($item['#help'])) {
+          if (!isset($item['#description'])) {
+            $item['#description'] = '';
+          }
+          $item['#description'] = $item['#help'] . "\n" . $item['#description'];
+          $item['#help'] = null;
+        }
+        if (isset($item['#type'])) {
+          if ($item['#type'] === 'textarea' || $item['#type'] === 'textfield') {
+            $item['#value'] = '';
+          }
+          if ($item['#type'] === 'checkboxes' || $item['#type'] === 'radios') {
+            $item['#value'] = '';
+          }
+        }
+      }
+    }
+  }
+  /**
+   * Returns a webform to be printed.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request object.
+   * @param string|null $library
+   *   The iframe JavaScript library.
+   * @param string|null $version
+   *   The iframe JavaScript library version.
+   *
+   * @return array
+   *   The webform rendered in a page template with only the content.
+   *
+   * @see page--webform-print.html.twig
+   */
+  public function page(Request $request, $library = NULL, $version = NULL) {
+    $webform = $this->requestHandler->getCurrentWebform();
+    $sourceEntity = $this->requestHandler->getCurrentSourceEntity(['webform']);
+    $webformArray = $webform->getElementsDecoded();
+/*
+    echo '<pre>';
+    print_r($webformArray);
+    die();*/
+    array_walk_recursive($webformArray, [$this, 'formatWebformElement']);
+    $this->traverseWebform($webformArray);
+
+    // Create a webform.
+    $webform->setElements($webformArray);
+    $build = [];
+    // Webform.
+    $build['webform'] = [
+      '#type' => 'webform',
+      '#webform' => $webform,
+      '#source_entity' => $sourceEntity,
+      '#prefix' => '<div class="webform-share-submission-form">',
+      '#suffix' => '</div>',
+    ];
+    return $build;
+
+  }
+
+}
