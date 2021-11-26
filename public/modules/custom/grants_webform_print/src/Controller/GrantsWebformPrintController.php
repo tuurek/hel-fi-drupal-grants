62cde285a4660cdb7e36e006c2181481eab46b23
Petri Leinonen <petri.leinonen@siili.com>
2021-11-26 09:29:55 +0200

    webform twig

--- public/modules/custom/grants_webform_print/src/Controller/GrantsWebformPrintController.php
+++ public/modules/custom/grants_webform_print/src/Controller/GrantsWebformPrintController.php
@@ -108,6 +108,7 @@ class GrantsWebformPrintController extends ControllerBase {
       '#source_entity' => $sourceEntity,
       '#prefix' => '<div class="webform-share-submission-form">',
       '#suffix' => '</div>',
+      '#theme' => 'page__webform_print',
     ];
     return $build;
 
