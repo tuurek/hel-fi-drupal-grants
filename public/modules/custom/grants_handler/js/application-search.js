1ecdfe45c4c912602fb1a75d601db30dca3eedd1
Petri Leinonen <petri.leinonen@siili.com>
2022-08-30 10:57:06 +0300

    AU-481: javascript fixes

--- public/modules/custom/grants_handler/js/application-search.js
+++ public/modules/custom/grants_handler/js/application-search.js
@@ -1,29 +1,29 @@
 (function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.GrantsHandlerApplicationSearchBehavior = {
+  Drupal.behaviors.GrantsHandlerApplicatiosSearchBehavior = {
     attach: function (context, settings) {
+      var fullListOptions = null;
+      var fullList = null;
+      if ($("#applications__list")[0]) {
+        fullListOptions = {
+          valueNames: ['application-list__item--name', 'application-list__item--status', 'application-list__item--number', 'application-list__item--submitted']
+        };
 
-
-
-      var fullListOptions = {
-        valueNames: [ 'application-list-item--name', 'application-list-item--status', 'application-list-item--number', 'application-list-item--submitted' ]
-      };
-      var fullList = new List('applications__list', fullListOptions);
-      $('#applications__list .application-list__count-value').html(fullList.update().matchingItems.length);
-
-      fullList.on('searchComplete', function(){
+        fullList = new List('applications__list', fullListOptions);
         $('#applications__list .application-list__count-value').html(fullList.update().matchingItems.length);
-      });
 
-      $('select.sort').change(function(){
-        var selection = $(this).val();
-        fullList.sort(selection);
-      });
+        fullList.on('searchComplete', function () {
+          $('#applications__list .application-list__count-value').html(fullList.update().matchingItems.length);
+        });
 
-      $('button.sort').click(function() {
-        fullList.sort($(this).data('sort'));
-      });
+        $('select.sort').change(function () {
+          var selection = $(this).val();
+          fullList.sort(selection);
+        });
 
-
+        $('button.sort').click(function () {
+          fullList.sort($(this).data('sort'));
+        });
+      }
     }
   };
 })(jQuery, Drupal, drupalSettings);
--- public/modules/custom/grants_oma_asiointi/js/application-draft-list.js
+++ public/modules/custom/grants_oma_asiointi/js/application-draft-list.js
@@ -1,44 +1,46 @@
 (function ($, Drupal, drupalSettings) {
 Drupal.behaviors.omaAsiointiFront = {
   attach: function (context, settings) {
-    var draftListOptions = {
-      valueNames: [ 'application-list-item--name', 'application-list-item--status', 'application-list-item--number', 'application-list-item--submitted' ]
-    };
-    var draftList = new List('oma-asiointi__drafts', draftListOptions);
-    $('#oma-asiointi__drafts .application-list__count-value').html(draftList.update().matchingItems.length);
-
-    draftList.on('searchComplete', function(){
+    if ($("#oma-asiointi__drafts")[0]) {
+      var draftListOptions = {
+        valueNames: ['application-list-item--name', 'application-list-item--status', 'application-list-item--number', 'application-list-item--submitted']
+      };
+      var draftList = new List('oma-asiointi__drafts', draftListOptions);
       $('#oma-asiointi__drafts .application-list__count-value').html(draftList.update().matchingItems.length);
-    });
 
-    $('select.sort').change(function(){
-      var selection = $(this).val();
-      draftList.sort(selection);
-    });
+      draftList.on('searchComplete', function () {
+        $('#oma-asiointi__drafts .application-list__count-value').html(draftList.update().matchingItems.length);
+      });
 
-    $('button.sort').click(function() {
-      draftList.sort($(this).data('sort'));
-    });
+      $('select.sort').change(function () {
+        var selection = $(this).val();
+        draftList.sort(selection);
+      });
 
-    var sentListOptions = {
-      valueNames: [ 'application-list-item--name', 'application-list-item--status', 'application-list-item--number', 'application-list-item--submitted' ]
-    };
-    var sentList = new List('oma-asiointi__sent', sentListOptions);
-    $('#oma-asiointi__sent .application-list__count-value').html(sentList.update().matchingItems.length);
-
-    sentList.on('searchComplete', function(){
+      $('button.sort').click(function () {
+        draftList.sort($(this).data('sort'));
+      });
+    }
+    if ($("#oma-asiointi__sent")[0]) {
+      var sentListOptions = {
+        valueNames: ['application-list__item--name', 'application-list__item--status', 'application-list__item--number', 'application-list__item--submitted']
+      };
+      var sentList = new List('oma-asiointi__sent', sentListOptions);
       $('#oma-asiointi__sent .application-list__count-value').html(sentList.update().matchingItems.length);
-    });
 
-    $('select.sort').change(function(){
-      var selection = $(this).val();
-      sentList.sort(selection);
-    });
+      sentList.on('searchComplete', function () {
+        $('#oma-asiointi__sent .application-list__count-value').html(sentList.update().matchingItems.length);
+      });
 
-    $('button.sort').click(function() {
-      sentList.sort($(this).data('sort'));
-    });
+      $('select.sort').change(function () {
+        var selection = $(this).val();
+        sentList.sort(selection);
+      });
 
+      $('button.sort').click(function () {
+        sentList.sort($(this).data('sort'));
+      });
+    }
   }
 };
 })(jQuery, Drupal, drupalSettings);
