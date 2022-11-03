08a5ac7a5ee4ac9975b1ad0d16a479f8ea5c6436
Petri Leinonen <petri.leinonen@siili.com>
2022-11-03 09:36:07 +0200

    import hdbt correctly

--- public/themes/custom/helfi_grant_applications/webpack.config.js
+++ public/themes/custom/helfi_grant_applications/webpack.config.js
@@ -1,188 +1,173 @@
-const isDev = process.env.NODE_ENV !== "production";
+const path = require('path');
+const glob = require('glob');
 
-const path = require("path");
-const glob = require("glob");
-const globImporter = require("node-sass-glob-importer");
+const FriendlyErrorsWebpackPlugin = require('@nuxt/friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const SvgToSprite = require('./webpack.svgToSprite');
+const { merge } = require('webpack-merge');
 
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
-
-module.exports = {
-  entry: {
-    styles: ["./src/scss/styles.scss"],
-    bundle: glob.sync("./src/js/**/*.js", {
-      ignore: [
-        // './src/js/some-example-component.js',
-      ],
-    }),
-    // "some-example-component": [
-    //   "./src/js/some-example-component.js",
-    //   "./src/scss/some-example-component.scss"
+// Handle entry points.
+const Entries = () => {
+  let entries = {
+    styles: ['./src/scss/styles.scss'],
+    // Special handling for some javascript or scss.
+    // 'some-component': [
+    //   './src/scss/some-component.scss',
+    //   './src/js/some-component.js',
     // ],
-  },
-  output: {
-    devtoolLineToLine: true,
-    path: path.resolve(__dirname, "dist"),
-    chunkFilename: "js/async/[name].chunk.js",
-    pathinfo: true,
-    filename: "js/[name].min.js",
-    publicPath: "../",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(config.js)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[path][name].[ext]",
-              outputPath: "./",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "media/[name].[ext]?[hash]",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "file-loader",
-          },
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-      {
-        test: /modernizrrc\.js$/,
-        loader: "expose-loader?Modernizr!webpack-modernizr-loader",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              name: "[name].[ext]?[hash]",
-            },
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: isDev,
-              importLoaders: 2,
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                config: path.join(__dirname, "postcss.config.js"),
-              },
-              sourceMap: isDev,
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: isDev,
-              sassOptions: {
-                importer: globImporter(),
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    modules: [path.join(__dirname, "node_modules")],
-    extensions: [".js", ".json"],
-  },
-  plugins: [
-    new FriendlyErrorsWebpackPlugin(),
-    new FixStyleOnlyEntriesPlugin(),
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname),
-    }),
-    new CopyPlugin({
-      patterns: [
+  };
+
+  const pattern = './src/js/**/*.js';
+  const ignore = [
+    // Some javascript what is needed to ignore and handled separately.
+    // './src/js/component-library.js'
+  ];
+
+  glob.sync(pattern, {ignore: ignore}).map((item) => {
+    entries[path.parse(item).name] = item }
+  );
+  return entries;
+};
+
+
+module.exports = (env, argv) => {
+
+  const isDev = (argv.mode === 'development');
+
+  // Set the base config
+  const config = {
+    entry() {
+      return Entries();
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      chunkFilename: 'js/async/[name].chunk.js',
+      pathinfo: true,
+      filename: 'js/[name].min.js',
+      publicPath: '../',
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          context: "./",
-          from: "node_modules/hyphenopoly/min/{Hyphenopoly_Loader,Hyphenopoly}.js",
-          to: `${path.resolve(__dirname, "dist")}/js/hyphenopoly/`,
-          force: true,
-          flatten: true,
+          test: /\.svg$/,
+          include: [
+            path.resolve(__dirname, 'src/icons')
+          ],
+          type: 'asset/resource',
         },
         {
-          context: "./",
-          from: "node_modules/hyphenopoly/min/patterns/{fi,sv,en-gb,ru}.wasm",
-          to: `${path.resolve(__dirname, "dist")}/js/hyphenopoly/patterns/`,
-          globOptions: {
-            extglob: true,
-          },
-          force: true,
-          flatten: true,
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+          type: 'javascript/auto',
+        },
+        {
+          test: /\.(css|sass|scss)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: isDev,
+                importLoaders: 2,
+                esModule: false,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                'postcssOptions': {
+                  'config': path.join(__dirname, 'postcss.config.js'),
+                },
+                sourceMap: isDev,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDev,
+                additionalData: "$debug_mode: " + isDev + ";",
+              },
+            },
+          ],
+          type: 'javascript/auto',
         },
       ],
-    }),
-    new SVGSpritemapPlugin([path.resolve(__dirname, "src/icons/**/*.svg")], {
-      output: {
-        filename: "./icons/sprite.svg",
-        svg: {
-          sizes: false,
-        },
+    },
+    resolve: {
+      modules: [
+        path.join(__dirname, 'node_modules'),
+      ],
+      extensions: ['.js', '.json'],
+    },
+    plugins: [
+      new SvgToSprite(
+        path.resolve(__dirname, 'src/icons/**/*.svg'),
+        'icons/grants-sprite.svg',
+        'icons.json'
+      ),
+      new FriendlyErrorsWebpackPlugin(),
+      new RemoveEmptyScriptsPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].min.css',
+      })
+    ],
+    watchOptions: {
+      aggregateTimeout: 300,
+    },
+    // Tell us only about the errors.
+    stats: 'errors-only',
+    // Suppress performance errors.
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    }
+  };
+
+  if (argv.mode === 'production') {
+    const TerserPlugin = require('terser-webpack-plugin');
+
+    const full_config = merge(config, {
+      mode: 'production',
+      devtool: false,
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              ecma: 2015,
+              format: {
+                comments: false,
+              },
+            },
+            extractComments: false,
+          }),
+        ],
       },
-      sprite: {
-        prefix: false,
-        gutter: 0,
-        generate: {
-          title: false,
-          symbol: true,
-          use: true,
-          view: "-view",
-        },
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].min.css",
-    }),
-  ],
-  watchOptions: {
-    aggregateTimeout: 300,
-  },
-  // Tell us only about the errors.
-  stats: "errors-only",
-  // Suppress performance errors.
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
+    });
+
+    return full_config;
+
+  } else if (argv.mode === 'development') {
+    const SourceMapDevToolPlugin = require('webpack/lib/SourceMapDevToolPlugin');
+
+    const full_config = merge(config, {
+      mode: 'development',
+      devtool: 'eval-source-map',
+      plugins: [
+        new SourceMapDevToolPlugin({
+          filename: '[file].map',
+          exclude: [/node_modules/, /images/, /spritemap/, /svg-sprites/],
+        })
+      ]
+    });
+
+    return full_config;
+
+  }
 };
